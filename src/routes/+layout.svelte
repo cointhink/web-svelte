1ea49c19259bc1fd59ff77c18a5dc4b6d7532a84
Don Park <donp@donp.org>
2023-08-04 11:24:10 -0700

    progress

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<div id="menubar">
+	<div>
+		<span class="logotext"> cointhink </span>
+	</div>
+
+	<div>Pools</div>
+
+	<div>login</div>
+</div>
+
+<slot />
+
+<style>
+	#menubar {
+		display: flex;
+		justify-content: space-between;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,6 @@
-<div>
-	<span class="logotext"> cointhink </span>
-</div>
-
 <div>+page.svelte</div>
 
 <style>
-	body {
-		background-color: black;
-	}
 	.logotext {
 		font-family: 'Montserrat', sans-serif;
 		margin: 0.2em 0;
