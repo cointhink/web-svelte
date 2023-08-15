25b8033f40ab8edd8d9546a9c9183656cdc8afd3
Don Park <donp@donp.org>
2023-08-15 10:13:32 -0700

    column updates

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,7 +4,7 @@ export const load = async ({ params }) => {
 	const url = 'https://cointhink.com/sql/pools';
 	const pools = await fetch(url).then((ps) => ps.json());
 	for (const pool of pools) {
-		const url = 'https://cointhink.com/sql/reserves?pool_index=eq.' + pool.index;
+		const url = 'https://cointhink.com/sql/reserves?contract_address=eq.' + pool.contract_address;
 		pool.reserves = await fetch(url).then((ps) => ps.json());
 	}
 	return { pools: pools };
