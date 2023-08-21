c66c2df78cbf07ce459d276ca79fc3c827a2b686
Don Park <donp@donp.org>
2023-08-21 09:51:50 -0700

    xy

--- src/lib/Poolrow.svelte
+++ src/lib/Poolrow.svelte
@@ -1,23 +1,26 @@
 <script lang="ts">
 	export let pool;
-	const reserves = pool.reserves[0]
-	const x = reserves.x / 10**pool.token0.decimals
-	const y = reserves.y / 10**pool.token1.decimals
+	const reserves = pool.reserves[0];
+	const x = reserves.x / 10 ** pool.token0.decimals;
+	const y = reserves.y / 10 ** pool.token1.decimals;
 </script>
 
-<style>
-#div { margin: 10px }
-</style>
-
 <div id="div">
-<!-- <div>{JSON.stringify(pool)}</div> -->
-<div>
-	pool
-	{pool.token0.symbol}/
-	{pool.token1.symbol}
-	<a href="https://v2.info.uniswap.org/pair/0x{pool.contract_address}">v2</a>
+	<!-- <div>{JSON.stringify(pool)}</div> -->
+	<div>
+		pool
+		{pool.token0.symbol}/
+		{pool.token1.symbol}
+		<a href="https://v2.info.uniswap.org/pair/0x{pool.contract_address}">v2</a>
+	</div>
+	<div>
+		#{reserves.block_number} y {y} / x {x} = {x / y}
+		{pool.token0.symbol}
+	</div>
 </div>
-<div>
-#{reserves.block_number} y {y} / x {x} = {x/y} {pool.token0.symbol}
-</div>
-</div>
+
+<style>
+	#div {
+		margin: 10px;
+	}
+</style>
