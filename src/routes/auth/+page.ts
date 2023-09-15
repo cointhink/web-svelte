dc501afd94b035a21e4c6c6755a14df169490558
Don Park <donp@donp.org>
2023-09-15 11:36:13 -0700

    cookie from the server

--- src/routes/auth/+page.ts
+++ src/routes/auth/+page.ts
@@ -4,10 +4,5 @@ export const load = async ({ fetch, params, url }) => {
 	const token = url.searchParams.get('token');
 	const auth_url = 'https://cointhink.com/api/auth/' + token;
 
-	return await fetch(auth_url).then((ps) => {
-		if (ps.status == 200) {
-			cookies.set('token', token, { path: '/' });
-		}
-		return { status: ps.status, response: ps.json() };
-	});
+	return await fetch(auth_url).then((ps) => ({ status: ps.status, response: ps.json() }));
 };
