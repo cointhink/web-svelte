bb0c1c2482360303864ac5e60601537bb1b6bfc7
Don Park <donp@donp.org>
2023-09-15 11:23:55 -0700

    d3 something

--- src/lib/D3Chart.svelte
+++ src/lib/D3Chart.svelte
@@ -18,35 +18,46 @@
 			.attr('width', width)
 			.attr('height', height);
 
-		const domain = 
-				d3.extent(reserves, function (d) {
-					return d.block_number;
-				})
-		const x = d3
+		const domain = d3.extent(reserves, function (d) {
+			return d.block_number;
+		});
+		const x = d3.scaleLinear().domain(domain).range([0, width]);
+
+		const y = d3
 			.scaleLinear()
-			.domain(domain)
-			.range([0, width]);
+			.domain([
+				0,
+				d3.max(reserves, function (d) {
+					return parseFloat(d.x);
+				})
+			])
+			.range([height - 22, 5]);
 
-    const y = d3.scaleLinear()
-      .domain([0, d3.max(reserves, function(d) { return parseFloat(d.x); })])
-      .range([ height-22, 5 ]);
+		svg
+			.append('g')
+			.attr('transform', `translate(0, ${height - 22})`)
+			.call(d3.axisBottom(x));
 
-		svg.append('g').attr('transform', `translate(0, ${height-22})`).call(d3.axisBottom(x));
+		sparkline.append('div').style('background-color', 'blue').text(JSON.stringify(reserves));
 
-		sparkline
-			.append('div')
-			.style('background-color', 'blue')
-			.text(JSON.stringify(reserves));
-
-	  svg.append("path")
-      .datum(reserves)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 11.5)
-      .attr("d", d3.line()
-        .x(function(d) { console.log(d); return x(d.block_number) })
-        .y(function(d) { return y(parseFloat(d.x)) })
-        )
+		svg
+			.append('path')
+			.datum(reserves)
+			.attr('fill', 'none')
+			.attr('stroke', 'steelblue')
+			.attr('stroke-width', 11.5)
+			.attr(
+				'd',
+				d3
+					.line()
+					.x(function (d) {
+						console.log(d);
+						return x(d.block_number);
+					})
+					.y(function (d) {
+						return y(parseFloat(d.x));
+					})
+			);
 	});
 </script>
 
