b8b073a9869dae8cfe32114dd7afb635cecfd804
Don Park <donp@donp.org>
2023-10-17 12:36:07 -0700

    python dev server with 200.html

---
+++ svelte-simple-server.py
@@ -0,0 +1,38 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import pathlib
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        path = pathlib.Path(self.path)
+        if path.suffix == '': 
+            self.path = '/200.html'
+            path = pathlib.Path(self.path)
+            #self.path += ".html" # try_files $url.html
+        print(f"path {path} suffix {path.suffix} newpath {self.path}")
+        try:
+            mime = extToMime(path.suffix)
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+            self.send_header('Content-type', mime)
+            self.end_headers()
+            self.wfile.write(bytes(file_to_open, 'utf-8'))
+        except:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'404 - Not Found')
+
+def extToMime(extension):
+    mime = 'text/plain'
+    match extension[1:]:
+        case 'css':
+            mime = 'text/css'
+        case 'html':
+            mime = 'text/html'
+        case 'js':
+            mime = 'text/javascript'
+    return mime
+
+httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
