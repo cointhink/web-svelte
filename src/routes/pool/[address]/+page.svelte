80539b6855bd375ab4c3bc3f48a87a94ba97d58c
Don Park <donp@donp.org>
2023-10-18 12:36:49 -0700

    pool name

--- src/routes/pool/[address]/+page.svelte
+++ src/routes/pool/[address]/+page.svelte
@@ -4,9 +4,9 @@
 	export let data;
 	let logs = [];
 	let pool = {};
-  let token0 = {decimals: 0};
-  let token1 = {decimals: 0};
-	
+	let token0 = { decimals: 0 };
+	let token1 = { decimals: 0 };
+
 	onMount(async () => {
 		const topic_swap = 'd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822';
 		const url1 =
@@ -24,15 +24,16 @@
 		}
 		const url2 = 'https://cointhink.com/sql/pools?contract_address=eq.' + data.params.address;
 		let pools = await fetch(url2).then((ps) => ps.json());
-		pool = pools[0]
+		pool = pools[0];
 		await moar(pool);
-		token0 = pool.token0
-		token1 = pool.token1
+		token0 = pool.token0;
+		token1 = pool.token1;
 	});
 </script>
 
 <div>
-	I am pool/{JSON.stringify(data.params.address)}
+	{token0.name}/{token1.name}
+	{data.params.address}
 </div>
 
 <div>
@@ -41,8 +42,8 @@
 
 {#each logs as log}
 	<pre>
-	coin0 in: {log.in0 / 10**token0.decimals} {token0.name}
-	coin1 in: {log.in1 / 10**token1.decimals} {token1.name}
+	coin0 in: {log.in0 / 10 ** token0.decimals} {token0.name}
+	coin1 in: {log.in1 / 10 ** token1.decimals} {token1.name}
 	coin0 out: {log.out0}
 	coin1 out: {log.out1}
 	</pre>
