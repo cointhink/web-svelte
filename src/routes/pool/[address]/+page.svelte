de1877ce6f9c0409bc0f8fbe0f7b076af19eb7a9
Don Park <donp@donp.org>
2023-10-18 12:34:43 -0700

    reactive svelte data yeah

--- src/routes/pool/[address]/+page.svelte
+++ src/routes/pool/[address]/+page.svelte
@@ -4,7 +4,9 @@
 	export let data;
 	let logs = [];
 	let pool = {};
-
+  let token0 = {decimals: 0};
+  let token1 = {decimals: 0};
+	
 	onMount(async () => {
 		const topic_swap = 'd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822';
 		const url1 =
@@ -24,6 +26,8 @@
 		let pools = await fetch(url2).then((ps) => ps.json());
 		pool = pools[0]
 		await moar(pool);
+		token0 = pool.token0
+		token1 = pool.token1
 	});
 </script>
 
@@ -37,8 +41,8 @@
 
 {#each logs as log}
 	<pre>
-	coin0 in: {log.in0} {pool.token0}
-	coin1 in: {log.in1}
+	coin0 in: {log.in0 / 10**token0.decimals} {token0.name}
+	coin1 in: {log.in1 / 10**token1.decimals} {token1.name}
 	coin0 out: {log.out0}
 	coin1 out: {log.out1}
 	</pre>
