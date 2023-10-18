9be4688dfc671e0996d89019a8c6a3ddefd46c47
Don Park <donp@donp.org>
2023-10-18 11:58:31 -0700

    event details

--- src/routes/pool/[address]/+page.svelte
+++ src/routes/pool/[address]/+page.svelte
@@ -1,12 +1,31 @@
 <script>
 	import { onMount } from 'svelte';
 	export let data;
+
+  for (const log of data.logs) {
+		let dparts = log.data.match(/.{1,64}/g)
+		log.in0 = parseInt(dparts[0],16)
+		log.in1 = parseInt(dparts[1],16)
+		log.out0 = parseInt(dparts[2],16)
+		log.out1 = parseInt(dparts[3],16)
+	}
 </script>
 
 <div>
 	I am pool/{JSON.stringify(data.params.address)}
 </div>
 
+<div>
+{data.logs.length} logs
+</div>
+
 {#each data.logs as log}
-	{JSON.stringify(log)}
+	<pre>
+	topic1: {log.topic1}
+	topic2: {log.topic2}
+	coin0 in: {log.in0}
+	coin1 in: {log.in1}
+	coin0 out: {log.out0}
+	coin1 out: {log.out1}
+	</pre>
 {/each}
--- src/routes/pool/[address]/+page.ts
+++ src/routes/pool/[address]/+page.ts
@@ -4,9 +4,16 @@ import { browser } from '$app/environment';
 export const load = async ({ fetch, params, url }) => {
 	let answer = { logs: [], params }; // placeholder for server-side build step
 	if (browser) {
-		const topic_swap = '0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822';
+    // event Swap(
+    //   address indexed sender,
+    //   uint amount0In,
+    //   uint amount1In,
+    //   uint amount0Out,
+    //   uint amount1Out,
+    //   address indexed to ); 
+		const topic_swap = 'd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822';
 		const url = 'https://cointhink.com/sql/logs?address=eq.' + params.address +
-			          '&topic0=' + topic_swap;
+			          '&topic0=eq.' + topic_swap;
 		answer.logs = await fetch(url).then((ps) => ps.json());
 	}
 	return answer;
