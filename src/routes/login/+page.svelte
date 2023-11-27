74e7cf496d5de9072679853ab181a7e0ffeb8969
Don Park <donp@donp.org>
2023-11-27 11:16:49 +0700

    login form statemachine

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,26 +2,60 @@
 	import Menubar from '$lib/Menubar.svelte';
 	import { PUBLIC_AUTH_URL } from '$env/static/public';
 
+	let form_state = 'start';
+
 	async function handleOnSubmit(form) {
 		const email_field = form.target[0]; //fragile. how to fix?
 		const email = form.target[0].value;
 		const url = PUBLIC_AUTH_URL + '/register/' + email;
 		console.log('handleOnSubmit', 'POST', url);
+		form_state = 'submit';
 		const response = await fetch(url, { method: 'POST' });
 		const json = await response.json();
-		console.log(json);
+		console.log('handleOnSubmit', json);
 		return false;
 	}
 </script>
 
 <div id="page">
-	<Menubar page_name="login" />
+	<Menubar page_name="" />
+
+	<div class="box">
+		<div class="bold">Sign in to defi.markets</div>
+		<div>Enter your email address to receive a magic-link login email.</div>
+		<form on:submit={handleOnSubmit}>
+			<div>Email</div>
+			<input type="email" name="email" />
+			{#if form_state == 'start'}
+				<div>
+					<button> Login </button>
+				</div>
+			{/if}
+			{#if form_state == 'submit'}
+				<div>Submitting...</div>
+			{/if}
+		</form>
+	</div>
 </div>
 
-<form on:submit={handleOnSubmit}>
-	<div>email address</div>
-	<input type="email" name="email" />
-	<div>
-		<button> Login </button>
-	</div>
-</form>
+<style>
+	.box {
+		width: 25em;
+		margin: auto;
+		border: solid;
+		border-radius: 1em;
+		padding: 1em;
+	}
+
+	.bold {
+		font-weight: bold;
+	}
+
+	.box > div {
+		padding: 1em 0;
+	}
+
+	form input {
+		font-size: 110%;
+	}
+</style>
