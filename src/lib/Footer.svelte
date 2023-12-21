ae59389346d50a6f6a3cd00910bcee3b723d2128
Don Park <donp@donp.org>
2023-12-21 15:00:57 +0700

    disclaimer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -4,4 +4,16 @@
 	onMount(async () => {});
 </script>
 
-<div />
+<div class="disclaimer">
+	Disclaimer: All content is for general information only, procured from third party sources. No
+	warranties of any kind in relation to our content are made, including but not limited to accuracy
+	and updatedness. No part of the content that we provide constitutes financial advice, legal advice
+	or any other form of advice meant for your specific reliance for any purpose.
+</div>
+
+<style>
+	.disclaimer {
+		margin: 3em 0;
+		font-size: 80%;
+	}
+</style>
