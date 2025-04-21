86d2d7fe2829f690bdeaad79208156722448ed31
Don Park <donp@donp.org>
2025-04-21 15:52:33 +0700

    display pool prices

--- src/routes/coin/[address]/+page.svelte
+++ src/routes/coin/[address]/+page.svelte
@@ -9,11 +9,15 @@
 	let pools = [];
 	let markets = [];
 	let token = {};
+	let tokens = {};
 
 	onMount(async () => {
 		token = await poollib.coin(data.params.address);
+		tokens[data.params.address] = token;
 		pools = await poollib.pools_for(data.params.address);
 		for (let idx = 0; idx < pools.length; idx++) {
+			tokens[pools[idx].token0] ||= await poollib.coin(pools[idx].token0);
+			tokens[pools[idx].token1] ||= await poollib.coin(pools[idx].token1);
 			pools[idx].reserves = await poollib.reserves(pools[idx].contract_address);
 		}
 	});
@@ -29,13 +33,30 @@
 </div>
 
 <div>
-	Markets
+	{pools.length} Markets
 	{#each pools as pool}
-		<div>
-			{pool.contract_address}
+		<div class="numbers">
+			0x{pool.contract_address}
 			{#if pool.reserves}
-				{pool.reserves.x / pool.reserves.y}
+				{#if pool.token1 == data.params.address}
+					{pool.reserves.x /
+						pool.reserves.y /
+						10 ** (tokens[pool.token1].decimals - tokens[pool.token0].decimals)}
+					{tokens[pool.token0].symbol}
+				{:else}
+					{pool.reserves.y /
+						pool.reserves.x /
+						10 ** (tokens[pool.token1].decimals - tokens[pool.token0].decimals)}
+					{tokens[pool.token1].symbol}
+				{/if}
 			{/if}
 		</div>
 	{/each}
 </div>
+
+<style>
+	.numbers {
+		font-family: monospace;
+		font-size: 110%;
+	}
+</style>
