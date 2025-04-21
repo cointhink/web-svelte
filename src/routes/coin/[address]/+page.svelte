ca863a8eaa9bcb80f6b4b4d6d702c0f54b96c4f9
Don Park <donp@donp.org>
2025-04-21 12:36:59 +0700

    find the pools for coin. get each pool reserves

--- src/routes/coin/[address]/+page.svelte
+++ src/routes/coin/[address]/+page.svelte
@@ -1,1 +1,38 @@
-coin
+<script>
+	import Menubar from '$lib/Menubar.svelte';
+	import * as poollib from '$lib/pool';
+	import { onMount } from 'svelte';
+	import { PUBLIC_SQL_URL } from '$env/static/public';
+
+	export let data;
+
+	let pools = [];
+	let markets = [];
+	let token = {};
+
+	onMount(async () => {
+		token = await poollib.coin(data.params.address);
+		pools = await poollib.pools_for(data.params.address);
+		for (let idx = 0; idx < pools.length; idx++) {
+			pools[idx].reserves = await poollib.reserves(pools[idx].contract_address);
+		}
+	});
+</script>
+
+<div id="page">
+	<Menubar page_name="Coin 0x{data.params.address}" />
+</div>
+
+<div>
+	{token.name}
+	({token.symbol})
+</div>
+
+<div>
+	Markets
+	{#each pools as pool}
+		<div>
+			{JSON.stringify(pool)}
+		</div>
+	{/each}
+</div>
