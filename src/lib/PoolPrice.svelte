11a5f4569957632abd987d65ea164abb83487c62
Don Park <donp@donp.org>
2025-05-07 12:18:37 +0700

    simplify

--- src/lib/PoolPrice.svelte
+++ src/lib/PoolPrice.svelte
@@ -14,12 +14,10 @@
 	let price_zero_pre;
 	let price_zero_post;
 	if (price < 0.1) {
-		if (price_sigfig.length > maxSigFigDig) {
-			price_zero_pre = price_sigfig.substring(0, 3);
-			let zero_stop = price_sigfig.length - maxSigFigDig;
-			zero_length = zero_stop - 2;
-			price_zero_post = price_sigfig.substring(zero_stop, price_sigfig.length);
-		}
+		price_zero_pre = price_sigfig.substring(0, 3);
+		let zero_stop = price_sigfig.length - maxSigFigDig;
+		zero_length = zero_stop - 2;
+		price_zero_post = price_sigfig.substring(zero_stop, price_sigfig.length);
 	}
 </script>
 
