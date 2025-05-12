24d42e9bf20b7a2052ae82b2428cf6d99b155aac
Don Park <donp@donp.org>
2025-05-12 08:43:41 +0700

    clickable coins and pools

--- src/routes/coin/[address]/+page.svelte
+++ src/routes/coin/[address]/+page.svelte
@@ -33,64 +33,69 @@
 	});
 </script>
 
+<Menubar page_name="Coin 0x{data.params.address}" />
+
 <div id="page">
-	<Menubar page_name="Coin 0x{data.params.address}" />
-</div>
+	<div class="bigname">
+		{#if loading}
+			loading 0x{data.params.address}
+		{:else if token}
+			{token.name}
+			({token.symbol})
+		{:else}
+			unknown coin 0x{data.params.address}
+		{/if}
+	</div>
 
-<div class="bigname">
-	{#if loading}
-		loading 0x{data.params.address}
-	{:else if token}
-		{token.name}
-		({token.symbol})
-	{:else}
-		unknown coin 0x{data.params.address}
-	{/if}
-</div>
-
-<div>
-	{#if loading}
-		loading markets for this coin...
-	{:else}
-		{pools.length} Markets
-	{/if}
-	{#each Object.entries(groups) as [target_address, pools]}
-		{#each pools as pool}
-			<div class="numbers">
-				0x{pool.contract_address}
-				{#if pool.reserves}
-					<span id="pool_pair">
-						{tokens[pool.token0].symbol}/{tokens[pool.token1].symbol}
-					</span>
-				{/if}
-				{#if pool.reserves}
-					<span class="bigger">
-						{#if pool.token1 == data.params.address}
-							<PoolPrice
-								reserves_x={pool.reserves.y}
-								reserves_y={pool.reserves.x}
-								token_x={tokens[pool.token0]}
-								token_y={tokens[pool.token1]}
-							/>
-						{:else}
-							<PoolPrice
-								reserves_x={pool.reserves.x}
-								reserves_y={pool.reserves.y}
-								token_x={tokens[pool.token1]}
-								token_y={tokens[pool.token0]}
-							/>
-						{/if}
-						inventory:
-						<PoolInventory reserves={pool.reserves.x} token={tokens[pool.token0]} />
-						<PoolInventory reserves={pool.reserves.y} token={tokens[pool.token1]} />
-					</span>
-				{/if}
-			</div>
+	<div>
+		{#if loading}
+			loading markets for this coin...
+		{:else}
+			{pools.length} Markets
+		{/if}
+		{#each Object.entries(groups) as [target_address, pools]}
+			{#each pools as pool}
+				<div class="numbers">
+					<a href="/pool/{pool.contract_address}">0x{pool.contract_address}</a>
+					{#if pool.reserves}
+						<span id="pool_pair">
+							<a href="/coin/{pool.token0}">{tokens[pool.token0].symbol}</a>/<a
+								href="/coin/{pool.token1}">{tokens[pool.token1].symbol}</a
+							>
+						</span>
+					{/if}
+					{#if pool.reserves}
+						<span class="bigger">
+							{#if pool.token1 == data.params.address}
+								<PoolPrice
+									reserves_x={pool.reserves.y}
+									reserves_y={pool.reserves.x}
+									token_x={tokens[pool.token0]}
+									token_y={tokens[pool.token1]}
+								/>
+							{:else}
+								<PoolPrice
+									reserves_x={pool.reserves.x}
+									reserves_y={pool.reserves.y}
+									token_x={tokens[pool.token1]}
+									token_y={tokens[pool.token0]}
+								/>
+							{/if}
+							inventory:
+							<PoolInventory reserves={pool.reserves.x} token={tokens[pool.token0]} />
+							<PoolInventory reserves={pool.reserves.y} token={tokens[pool.token1]} />
+						</span>
+					{/if}
+				</div>
+			{/each}
 		{/each}
-	{/each}
+	</div>
 </div>
 
 <style>
+	#page a {
+		text-decoration: none;
+	}
 	#pool_pair {
 		width: 7em;
 		display: inline-block;
