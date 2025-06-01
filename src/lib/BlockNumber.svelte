8f4100ece33acf6c56d06d6fbfe0766072a00d58
Don Park <donp@donp.org>
2025-06-01 10:42:48 +0700

    blocknumber time units

--- src/lib/BlockNumber.svelte
+++ src/lib/BlockNumber.svelte
@@ -1,10 +1,28 @@
 <script>
 	export let last_block;
 	export let block_number;
+
+	let block_time_number;
+	let block_time_unit;
+
+	const block_time_seconds = (last_block.number - block_number) * 12;
+	if (block_time_seconds > 60) {
+		block_time_number = block_time_seconds / 60;
+		block_time_unit = 'mins';
+	}
+	if (block_time_seconds > 60 * 60) {
+		block_time_number = block_time_seconds / 60 / 60;
+		block_time_unit = 'hrs';
+	}
+	if (block_time_seconds > 60 * 60 * 24) {
+		block_time_number = block_time_seconds / 60 / 60 / 24;
+		block_time_unit = 'days';
+	}
 </script>
 
 <span class="blockno">#{block_number}</span>
-{(((last_block.number - block_number) * 12) / 60).toFixed(1)} min old
+{block_time_number.toFixed(1)}
+{block_time_unit} old
 
 <style>
 	.blockno {
